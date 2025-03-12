494532659150ddcc657930eb039bb9950b7cbdfe
vishal08051996 <104846074+vishal08051996@users.noreply.github.com>
2025-03-12 17:23:48 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# Get the API key
+api_key = os.getenv("GROQ_API_KEY")
+
+# Print the key (for testing purposes)
+print(f"Your API Key is: {api_key}")
